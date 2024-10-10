e94c5b6de7994f53f792caf1ad4ae2b33bcbe75c
Valeria <58702695+materias@users.noreply.github.com>
2024-10-10 17:55:25 +0400

    Creating Header and Footer

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="center">
+                <h1>Cost Calculator</h1>
+                <img loading="lazy" src="../assets/" alt="Screenshot 1" />
+                <h2>Use the calculator below to estimate your current costs and discover the potential savings by switching to %brand%</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header
